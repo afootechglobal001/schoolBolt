5f11be8f90364c5d43b1b744008c7131e372eb9e
Emmytech <seunemmanuel107@gmail.com>
2025-03-27 00:37:10 +0100

    update 1 @ 27-03-2025 on admin portal class module

--- portal/js/branch/useClass.js
+++ portal/js/branch/useClass.js
@@ -52,6 +52,7 @@ function _fetchBranchDepartmentClass() {
                         no++;
                         const department = fetch[i];
                         const departmentName = department.departmentData.departmentName;
+						const departmentId = department.departmentData.departmentId;
                         const classData = department.classData;
 
                         text += `
@@ -84,68 +85,71 @@ function _fetchBranchDepartmentClass() {
                                             </thead>
                                             <tbody>`;
 
-								let sn = 0; 
-								if (classData.length > 0) {
-									for (let j = 0; j < classData.length; j++) {
-										const classInfo = classData[j];
-										const className = classInfo.className;
-										const armData = classInfo.armData;
+                        let sn = 0; 
+                        if (classData.length > 0) {
+                            for (let j = 0; j < classData.length; j++) {
+                                const classInfo = classData[j];
+                                const className = classInfo.className;
+								const classId = classInfo.classId;
+                                const armData = classInfo.armData;
 
-										if (armData.length > 0) {
-											for (let k = 0; k < armData.length; k++) {
-												sn++;
-												const arm = armData[k];
+                                if (armData.length > 0) {
+                                    for (let k = 0; k < armData.length; k++) {
+                                        sn++;
+                                        const armInfo = armData[k];
+                                        const arm = armInfo.armName;
+										const armId = armInfo.armId;
+                                        const teacherData = armInfo.teacherData;
 
-												text += `
-													<tr class="tb-row">
-														<td>${sn}</td>
-														<td>${departmentName}</td>
-														<td>${className} ${arm.armName}</td>
+                                        text += `
+                                            <tr class="tb-row">
+                                                <td>${sn}</td>
+                                                <td>${departmentName}</td>
+                                                <td>${className} ${arm}</td>`;
+
+												if (teacherData && typeof teacherData) {
+													const fullname = teacherData.fullname;
+													const emailAddress = teacherData.emailAddress;
+													const profilePix = teacherData.profilePix ? teacherData.profilePix : "default.jpg";
+
+													text += `
 														<td>
 															<div class="text-back-div">
 																<div class="image-div general-passport">
-																	<img src="${websiteUrl}/uploaded_files/staffPix/teacher3.png" alt="MR AHMED ODELAKIN"/>
+																	<img src="${websiteUrl}/uploaded_files/staffPix/${profilePix}" alt="${fullname}"/>
 																</div>
 
 																<div class="text-div">
-																	<div class="first-class">MR AHMED ODELAKIN</div>
-																	<div class="second-class">ahmedolumide20@gmail.com</div>
+																	<div class="first-class">${fullname}</div>
+																	<div class="second-class">${emailAddress}</div>
 																</div>
 															</div>
-														</td>
-														<td><button class="btn view-btn" title="Click to edit assign class teacher" onclick="_getForm({page: 'assign_staff', layer:2, url: adminPortalLocalUrl});"><i class="bi-bookmark-check"></i> ASSIGN</button></td>
-													</tr>`;
-											}
-										} else {
-											sn++;
-											text += `
-												<tr class="tb-row">
-													<td>${sn}</td>
-													<td>${departmentName}</td>
-													<td>${className} (No Arm)</td>
-													<td>
-														<div class="text-back-div">
-															<div class="image-div general-passport">
-																<img src="${websiteUrl}/uploaded_files/staffPix/teacher3.png" alt="MR AHMED ODELAKIN"/>
-															</div>
+														</td>`;
+												} else {
+													text += '<td>No Teacher Assigned</td>';
+												}
 
-															<div class="text-div">
-																<div class="first-class">MR AHMED ODELAKIN</div>
-																<div class="second-class">ahmedolumide20@gmail.com</div>
-															</div>
-														</div>
-													</td>
-													<td></td>
-												</tr>`;
-										}
-									}
-								} 
-								text += `
-											</tbody>
-										</table>
-									</div>
-								</div>
-							</div>`;
+												text += `
+                                                <td><button class="btn view-btn" title="Click to edit assign class teacher" onclick="_fetchClassTeacher('${departmentId}','${classId}','${armId}');"><i class="bi-bookmark-check"></i> ASSIGN</button></td>
+                                            </tr>`;
+                                    }
+                                } else {
+                                    sn++;
+                                    text += `
+                                        <tr class="tb-row">
+                                            <td>${sn}</td>
+                                            <td>${departmentName}</td>
+                                            <td>${className} (No Arm)</td>
+                                            <td colspan="2">No Teacher Assigned</td>
+                                        </tr>`;
+                                }
+                            }
+                        } 
+                        text += `</tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>`;
                     }
                     $('#pageContent').html(text);
                 } else {
@@ -175,4 +179,89 @@ function _fetchBranchDepartmentClass() {
         console.error("Error: ", error);
         _actionAlert('An unexpected error occurred! Please try again.', false);
     }
+}
+
+
+function _fetchClassTeacher(departmentId, classId, armId) {
+	$("#get-more-div-secondary").css({'display': 'flex','justify-content': 'center','align-items': 'center'}) .fadeIn(500);
+	try {
+		$.ajax({
+			type: "GET",
+			url: `${endPoint}/admin/branch/class/fetch-class-teacher?branchId=${getEachBranchDetailsSession.branchId}&departmentId=${departmentId}&classId=${classId}&armId=${armId}`,
+			dataType: "json", 
+			cache: false,
+			headers: getAuthHeaders(true),
+			success: function(info) {
+				if (info.success) {
+					sessionStorage.setItem("getClassTeacherSession", JSON.stringify(info));
+					_getForm({page: 'assign_staff', layer:2, url: adminPortalLocalUrl});
+				} else {
+					const response = info.response;
+					if (response < 100) {
+						_logOut();
+					}    
+				}
+			},
+			error: function(textStatus, errorThrown) {
+				console.error("AJAX Error: ", textStatus, errorThrown);
+				_actionAlert('An error occurred while fetching data! Please try again.', false);
+			}
+		});
+	} catch (error) {
+		_alertClose();
+		console.error("Error: ", error);
+		_actionAlert('An unexpected error occurred! Please try again.', false);
+	}
+}
+
+
+function addClassTeacher() {
+	try {
+		const staffId = $('#staffId').val();
+
+		$('#staffId').removeClass('issue');
+
+		if (!staffId) {
+			$('#staffId').addClass('issue');
+			_actionAlert('Select class teacher to continue', false);
+			return;
+		}
+
+		if (confirm("Confirm!!\n\n Are you sure to PERFORM THIS ACTION?")) {
+			const btn_text = $("#submitBtn").html();
+			$("#submitBtn").html('<img src="' + websiteUrl + '/images/loading.gif" width="12px" alt="Loading"/>');
+			$("#submitBtn").prop("disabled", true);
+
+			const formData = {
+				"staffId": staffId
+			};
+
+			$.ajax({
+				type: "POST",
+				url: `${endPoint}/admin/branch/class/add-class-teacher?branchId=${getEachBranchDetailsSession.branchId}&departmentId=${getClassTeacherSession.departmentData.departmentId}&classId=${getClassTeacherSession.classData.classId}&armId=${getClassTeacherSession.armData.armId}`,
+				data: JSON.stringify(formData),
+				dataType: "json", 
+				cache: false,
+				headers: getAuthHeaders(true),
+				processData: false,
+				success: function (data) {
+				if (data.success) {
+					_actionAlert(data.message, true);
+					_getActiveBranchPage({divid:'branch_department_class', page: 'branch_department_class', url: adminPortalLocalUrl});
+					_alertClose(2);
+				} else {
+					_actionAlert(data.message, false);
+				}
+				$("#submitBtn").html(btn_text).prop("disabled", false);
+			},
+				error: function (error) {
+					_actionAlert('An error occurred while processing your request! Please Try Again', false);
+					$("#submitBtn").html(btn_text).prop("disabled", false);
+				}
+			});
+		}
+	} catch (error) {
+		_actionAlert('An unexpected error occurred! Please Try Again', false);
+		$("#submitBtn").prop("disabled", false);
+	}
 }
