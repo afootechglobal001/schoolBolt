f29c5012e7826d388f48dbad2ec4ff2838a53620
Emmytech <seunemmanuel107@gmail.com>
2025-03-28 03:24:19 +0100

    update 1 @ 28-03-2025 on admin portal subject module on branch

--- portal/config/admin/operations/branch-operation/subject-content.php
+++ portal/config/admin/operations/branch-operation/subject-content.php
@@ -27,14 +27,14 @@
                 </script>
             </div>
 
-            <button class="btn" id="submit_btn" title="Proceed Request" onclick="_getActiveBranchPage({divid:'branch_subject_page', page: 'branch_subject_page', url: adminPortalLocalUrl});">PROCEED <i class="bi-arrow-right"></i> </button>
+            <button class="btn" id="submitBtn" title="Proceed Request" onclick="_proceedFetchBranchSubject();">PROCEED <i class="bi-arrow-right"></i> </button>
         </div>
     </div>
 <?php } ?>
 
 <?php if ($page == 'branch_subject_page') { ?>
     <div class="alert alert-success top-alert-div animated fadeIn">
-        <div><span><i class="bi-person-bounding-box"></i></span> BRANCH SUBJECT'S LIST ---- <span id="session">2024/2025</span> - <span id="termName">FIRST TEAM</span> - <span id="departmentName3">NURSERY</span> - <span id="className2">NURSERY 1</span></div>
+        <div><span><i class="bi-person-bounding-box"></i></span> BRANCH SUBJECT'S LIST ---- <span id="session">Loading...</span> - <span id="termName">Loading...</span> - <span id="departmentName3">Loading...</span> - <span id="className2">Loading...</span></div>
         <div class="btn-container">
             <button class="btn" title="PRINT RECORDS" id="" onclick=""><i class="bi-printer"></i> PRINT</button>
             <button class="btn" title="EXPORT RECORDS" id="" onclick=""><i class="bi-file-earmark-excel"></i> EXPORT</button>
@@ -43,113 +43,13 @@
 
     <div class="table-div animated fadeIn">
         <table class="table" cellspacing="0" style="width:100%" id="pageContent">
-            <thead>
-                <tr class="tb-col">
-                    <th>sn</th>
-                    <th>Session</th>
-                    <th>Term</th>
-                    <th>Department</th>
-                    <th>Class</th>
-                    <th>Subject</th>
-                    <th>Subject Teacher</th>
-                    <th>Edit</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr class="tb-row">
-                    <td>1</td>
-                    <td>2024/2025</td>
-                    <td>FIRST TERM</td>
-                    <td>NURSERY</td>
-                    <td>NURSERY 1</td>
-                    <td>BASIC SCIENCE</td>
-                    <td>
-                        <div class="text-back-div">
-                            <div class="image-div general-passport">
-                                <img src="<?php echo $websiteUrl?>/uploaded_files/staffPix/default.jpg" alt="MR EMMANUEL PAUL"/>
-                            </div>
-
-                            <div class="text-div">
-                                <div class="first-class">MR EMMANUEL PAUL</div>
-                                <div class="second-class">seunemmanuel107@gmail.com</div>
-                            </div>
-                        </div>
-                    </td>
-                    <td><button class="btn view-btn" title="Click to edit assign class teacher" onclick="_getForm({page: 'assign_subject_staff', layer:2, url: adminPortalLocalUrl});"><i class="bi-bookmark-check"></i> ASSIGN</button></td>
-                </tr>
-
-                <tr class="tb-row">
-                    <td>2</td>
-                    <td>2024/2025</td>
-                    <td>FIRST TERM</td>
-                    <td>NURSERY</td>
-                    <td>NURSERY 1</td>
-                    <td>BASIC TECHNOLOGY</td>
-                    <td>
-                        <div class="text-back-div">
-                            <div class="image-div general-passport">
-                                <img src="<?php echo $websiteUrl?>/uploaded_files/staffPix/default.jpg" alt="MR EMMANUEL PAUL"/>
-                            </div>
-
-                            <div class="text-div">
-                                <div class="first-class">MR EMMANUEL PAUL</div>
-                                <div class="second-class">seunemmanuel107@gmail.com</div>
-                            </div>
-                        </div>
-                    </td>
-                    <td><button class="btn view-btn" title="Click to edit assign class teacher" onclick=""><i class="bi-bookmark-check"></i> ASSIGN</button></td>
-                </tr>
-
-                <tr class="tb-row">
-                    <td>3</td>
-                    <td>2024/2025</td>
-                    <td>FIRST TERM</td>
-                    <td>NURSERY</td>
-                    <td>NURSERY 1</td>
-                    <td>CHRISTIAN RELIGIOUS STUDIES</td>
-                    <td>
-                        <div class="text-back-div">
-                            <div class="image-div general-passport">
-                                <img src="<?php echo $websiteUrl?>/uploaded_files/staffPix/default.jpg" alt="MR EMMANUEL PAUL"/>
-                            </div>
-
-                            <div class="text-div">
-                                <div class="first-class">MR EMMANUEL PAUL</div>
-                                <div class="second-class">seunemmanuel107@gmail.com</div>
-                            </div>
-                        </div>
-                    </td>
-                    <td><button class="btn view-btn" title="Click to edit assign class teacher" onclick=""><i class="bi-bookmark-check"></i> ASSIGN</button></td>
-                </tr>
-
-                <tr class="tb-row">
-                    <td>4</td>
-                    <td>2024/2025</td>
-                    <td>FIRST TERM</td>
-                    <td>NURSERY</td>
-                    <td>NURSERY 1</td>
-                    <td>CIVIC EDUCATION</td>
-                    <td>
-                        <div class="text-back-div">
-                            <div class="image-div general-passport">
-                                <img src="<?php echo $websiteUrl?>/uploaded_files/staffPix/default.jpg" alt="MR EMMANUEL PAUL"/>
-                            </div>
-
-                            <div class="text-div">
-                                <div class="first-class">MR EMMANUEL PAUL</div>
-                                <div class="second-class">seunemmanuel107@gmail.com</div>
-                            </div>
-                        </div>
-                    </td>
-                    <td><button class="btn view-btn" title="Click to edit assign class teacher" onclick=""><i class="bi-bookmark-check"></i> ASSIGN</button></td>
-                </tr>
-            </tbody>
+            <script>_fetchBranchSubjects();</script>
         </table>
     </div>
 <?php } ?>
 
 <?php if ($page=='assign_subject_staff') { ?>
+    <script> getSubjectTeacherSession = JSON.parse(sessionStorage.getItem("getSubjectTeacherSession"));</script>
 
     <div class="slide-form-div" data-aos="fade-left" data-aos-duration="900">
         <div class="title-panel-div">
@@ -169,23 +69,15 @@
                     <div class="alert-list-div">
                         <div class="alert-list">
                             <div>Branch:</div>
-                            <div><span id="branchName">AFOOTECH GLOBAL INSTITUTE</span></div>
-                        </div>
-                        <div class="alert-list">
-                            <div>Session:</div>
-                            <div><span id="branchName">2024/2025</span></div>
-                        </div>
-                        <div class="alert-list">
-                            <div>Term:</div>
-                            <div><span id="branchName">FIRST TERM</span></div>
+                            <div><span id="branchName"><script>$("#branchName").html(getSubjectTeacherSession.branchData.branchName);</script></span></div>
                         </div>
                         <div class="alert-list">
                             <div>Department:</div>
-                            <div><span id="departmentName">NURSERY</span></div>
+                            <div><span id="departmentName"><script>$("#departmentName").html(getSubjectTeacherSession.departmentData.departmentName);</script></span></div>
                         </div>
                         <div class="alert-list">
                             <div>Class:</div>
-                            <div><span id="level">NURSERY 1</span></div>
+                            <div><span id="className"><script>$("#className").html(getSubjectTeacherSession.classData.className);</script></span></div>
                         </div>
                     </div>
                 </div>
@@ -194,14 +86,16 @@
                     <script>
                         selectField({
                             id: 'staffId',
-                            title: 'Select Class Teacher'
+                            title: 'Select Class Teacher',
+                            fieldValue: getSubjectTeacherSession?.data[0]?.staffId ?? '',
+                            fieldLabel: getSubjectTeacherSession?.data[0]?.teacherData?.fullname ?? ''
                         });
-                        _getSelectClassTeachers('staffId');
+                        _getSelectSubjectTeachers('staffId');
                     </script>
                 </div>
 
                 <div>
-                    <button class="btn" title="SUBMIT" id="submitBtn" onclick=""> <i class="bi-check"></i> SUBMIT </button>
+                    <button class="btn" title="SUBMIT" id="submitBtn" onclick="allocateSubjectTeacher();"> <i class="bi-check"></i> SUBMIT </button>
                 </div>
             </div>
         </div>
