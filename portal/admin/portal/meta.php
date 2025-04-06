0ed241e954341c67bf976f6eb388b6f5031b138b
Emmytech <seunemmanuel107@gmail.com>
2025-04-06 23:09:50 +0100

    update 1 @ 06-04-2025 on report print and export for student list

--- portal/admin/portal/meta.php
+++ portal/admin/portal/meta.php
@@ -42,3 +42,4 @@
 <script src="<?php echo $websiteUrl?>/js/branch/class/useClass.js?v=<?php echo $codeVersion?>"></script>
 <script src="<?php echo $websiteUrl?>/js/branch/department/useDepartment.js?v=<?php echo $codeVersion?>"></script>
 <script src="<?php echo $websiteUrl?>/js/branch/subject/useSubject.js?v=<?php echo $codeVersion?>"></script>
+<script src="<?php echo $websiteUrl?>/js/reports/print-student-by-class.js?v=<?php echo $codeVersion?>"></script>
