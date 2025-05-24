8c0ccec313975efc41dedb77cd8b381587c3df8e
Mike Afolabi <afolabi.o@eleadersnetwork.com>
2025-05-24 22:19:25 +0100

    fix: updates 1 on parent payment  @ 24-05-2025

--- portal/config/constants.php
+++ portal/config/constants.php
@@ -4,10 +4,10 @@
 	$appName='schoolBolt'; 
 	$clientName='SchoolBolt Edu System';
 	/////////////////////////////////////////////////////////////////////////////////
+	$clientWebsiteUrl='http://localhost/projects/schoolbolt';
+	$clientWebsiteContactUsUrl=$clientWebsiteUrl.'/contact-us';
 	//$websiteUrl='https://schoolbolt.com/portal'; /// For Live Server Url //
 	$websiteUrl='http://localhost/projects/schoolbolt/portal';
-	$clientWebsiteUrl='http://localhost/projects/schoolbolt';
-	$clientWebsiteContactUsUrl='http://localhost/projects/schoolbolt/contact-us';
 	//$websitePath = $_SERVER['DOCUMENT_ROOT'].'/portal';
 	$websitePath = $_SERVER['DOCUMENT_ROOT'].'/projects/schoolbolt/portal'; //dirname(__FILE__);
 	$codeVersion= date('Ymdhis');
