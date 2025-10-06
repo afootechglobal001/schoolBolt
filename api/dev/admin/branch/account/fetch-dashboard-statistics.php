2bb3d008194f1618213fee33713b445bca024efa
Mike Afolabi <afolabi.o@eleadernetwork.com>
2025-10-06 06:48:01 +0100

    update on dashboard bars

--- api/dev/admin/branch/account/fetch-dashboard-statistics.php
+++ api/dev/admin/branch/account/fetch-dashboard-statistics.php
@@ -14,13 +14,10 @@ $branchId = trim($_GET['branchId']);
 
 $dataQuery = mysqli_query($conn,"
     SELECT
-        (SELECT COUNT(*) FROM BRANCHES_TAB WHERE $clientIds AND branchId='$branchId' AND statusId=1) AS total_active_branch_count,
         (SELECT COUNT(*) FROM STAFF_TAB WHERE $clientIds AND branchId='$branchId' AND statusId=1) AS total_active_staff_count,
         (SELECT COUNT(*) FROM STUDENTS_TAB WHERE $clientIds AND branchId='$branchId' AND statusId=1) AS total_active_student_count,
         (SELECT COUNT(*) FROM STUDENTS_TAB WHERE $clientIds AND branchId='$branchId' AND statusId=12) AS total_alumni_student_count,
-        (SELECT COUNT(*) FROM DEPARTMENTS_TAB WHERE $clientIds AND branchId='$branchId' AND statusId=1) AS total_active_department_count,
-        (SELECT COUNT(*) FROM CLASSES_TAB WHERE $clientIds AND branchId='$branchId' AND statusId=1) AS total_active_class_count,
-        (SELECT COUNT(*) FROM SUBJECTS_TAB WHERE $clientIds AND branchId='$branchId' AND statusId=1) AS total_active_subject_count
+        (SELECT COUNT(*) FROM BRANCH_DEPARTMENTS_TAB WHERE $clientIds AND branchId='$branchId') AS total_active_department_count
 ");
 
 if (!$dataQuery) {
@@ -32,7 +29,6 @@ $response = [
     'response'=> 200,
     'success'=> true,
     'data'=>  array(),
-    'staffMatrix' => array()
 ];  
 
 while ($fetchDataQuery = mysqli_fetch_assoc($dataQuery)) {
