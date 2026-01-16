1f51720441034436914fb11560689be8b7aa9500
Mike Afolabi <afolabi.o@eleadernetwork.com>
2026-01-16 18:36:53 +0100

    update on parent login

--- portal/js/parent/useAuth.js
+++ portal/js/parent/useAuth.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
   });
 });
 
-
 function _getSelectParentType(fieldId) {
   const data = [
     {
@@ -74,12 +73,14 @@ function _counDownOtp(timer) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Confirm Login Email ///
-function _confirmLoginEmail(isResend = false){
-  let parentProceedLoginSession = JSON.parse(localStorage.getItem("parentProceedLoginSession"));
-	try {
-		////////get all needed values////////////
-		let issueCount = 0;
-		let parentTypeId = $("#parentTypeId").val()?.trim();
+function _confirmLoginEmail(isResend = false) {
+  let parentProceedLoginSession = JSON.parse(
+    localStorage.getItem("parentProceedLoginSession")
+  );
+  try {
+    ////////get all needed values////////////
+    let issueCount = 0;
+    let parentTypeId = $("#parentTypeId").val()?.trim();
     let email = $("#email").val()?.trim();
 
     // Use session values when resending ///
@@ -88,28 +89,27 @@ function _confirmLoginEmail(isResend = false){
       email = parentProceedLoginSession.email;
     }
 
-		///// empty field validation//////////
-		if (!isResend) {
+    ///// empty field validation//////////
+    if (!isResend) {
       issueCount += _validateEmptyValue("parentTypeId", "PARENT TYPE");
       issueCount += _validateEmptyValue("email", "EMAIL ADDRESS");
     }
 
-		if (issueCount > 0) return;
+    if (issueCount > 0) return;
 
-		// Gather form data
-		const formData = { parentTypeId, email };
+    // Gather form data
+    const formData = { parentTypeId, email };
 
-		_confirmLoginCallback(formData, isResend);
-		
-	} catch (error) {
-		console.error("Error:", error);
-		_callCatchError(() => _confirmLoginEmail(isResend = false));
-	}
+    _confirmLoginCallback(formData, isResend);
+  } catch (error) {
+    console.error("Error:", error);
+    _callCatchError(() => _confirmLoginEmail((isResend = false)));
+  }
 }
 
 ///  Confirm Login Callback ///
 function _confirmLoginCallback(formData, isResend) {
-	///// get btn text/////
+  ///// get btn text/////
   let btnText = "";
   if (!isResend) {
     btnText = $("#proceedLoginBtn").html();
@@ -117,116 +117,109 @@ function _confirmLoginCallback(formData, isResend) {
   } else {
     _showLoader("Resending OTP, please wait...");
   }
-	//// call endpoint //////
-	_callRawEndPoints({
-		url: `parent/auth/verification`,
-		formData,
-	})
+  //// call endpoint //////
+  _callRawEndPoints({
+    url: `parent/auth/verification`,
+    formData,
+  })
     .then((response) => {
-  if (response.success) {
-    _showCustomConfirm({
-      callback: () => {
+      if (response.success) {
         if (!isResend) {
-          localStorage.setItem("parentProceedLoginSession", JSON.stringify(response));
+          _btnDisable("proceedLoginBtn", btnText, false);
+          localStorage.setItem(
+            "parentProceedLoginSession",
+            JSON.stringify(response)
+          );
           _showLoader("OTP Sent Successfully!. Please wait...");
           window.location.href = parentOtpVerificationUrl;
         } else {
           _hideLoader();
           _counDownOtp(30);
         }
-      },
-      title: 'Success!',
-      message: 'Your OTP has been successfully sent to your email. Please proceed to enter the code to continue.',
-      alertType: 'success',
-      trueActionBtnText: 'Proceed.',
-      closeOnOverlayClick: false,
+      } else {
+        _btnDisable("proceedLoginBtn", btnText, false);
+        _showCustomConfirm({
+          title: "Invalid Credentials!",
+          message: response.message,
+          alertType: "error",
+          trueActionBtnText: "OK",
+          closeOnOverlayClick: true,
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      _callAjaxError(() => _confirmLoginCallback(formData)); // retry if needed
+      if (!isResend) {
+        _btnDisable("proceedLoginBtn", btnText, false);
+      } else {
+        _hideLoader();
+      }
     });
-
-    if (!isResend) {
-      _btnDisable("proceedLoginBtn", btnText, false);
-    } else {
-      _hideLoader();
-    }
-    
-		} else {
-			_btnDisable("proceedLoginBtn", btnText, false);
-			_showCustomConfirm({
-				title: "PROCEED LOGIN",
-				message: response.message,
-				alertType: "warning",
-				trueActionBtnText: "OK",
-        closeOnOverlayClick: true,
-			});
-		}
-  })
-  .catch((error) => {
-		console.error("Error:", error);
-		_callAjaxError(() => _confirmLoginCallback(formData)); // retry if needed
-    if (!isResend) {
-		  _btnDisable("proceedLoginBtn", btnText, false);
-    } else {
-      _hideLoader();
-    }
-  });
 }
 
 /// Proceed To Login ///
-function _proceedToLogin(){
-  let parentProceedLoginSession = JSON.parse(localStorage.getItem("parentProceedLoginSession"));
-	try {
-		////////get all needed values////////////
-		let issueCount = 0;
-		const otp = $("#otp").val();
+function _proceedToLogin() {
+  let parentProceedLoginSession = JSON.parse(
+    localStorage.getItem("parentProceedLoginSession")
+  );
+  try {
+    ////////get all needed values////////////
+    let issueCount = 0;
+    const otp = $("#otp").val();
 
-		///// empty field validation//////////
-		issueCount += _validateEmptyValue("otp", "OTP");
+    ///// empty field validation//////////
+    issueCount += _validateEmptyValue("otp", "OTP");
 
-		if (issueCount > 0) return;
+    if (issueCount > 0) return;
 
-		// Gather form data
-		const formData = { otp, parentTypeId: parentProceedLoginSession.parentTypeId, email: parentProceedLoginSession.email };
+    // Gather form data
+    const formData = {
+      otp,
+      parentTypeId: parentProceedLoginSession.parentTypeId,
+      email: parentProceedLoginSession.email,
+    };
 
-		_proceedToLoginCallback(formData);
-		
-	} catch (error) {
-		console.error("Error:", error);
-		_callCatchError(() => _proceedToLogin());
-	}
+    _proceedToLoginCallback(formData);
+  } catch (error) {
+    console.error("Error:", error);
+    _callCatchError(() => _proceedToLogin());
+  }
 }
 
 /// Proceed To Login Callback ///
 function _proceedToLoginCallback(formData) {
-	///// get btn text/////
-	const btnText = $("#submitBtn").html();
-	_btnDisable("submitBtn", btnText, true);
-	
-	//// call endpoint //////
-	_callRawEndPoints({
-		url: `parent/auth/login`,
-		formData,
-	})
+  ///// get btn text/////
+  const btnText = $("#submitBtn").html();
+  _btnDisable("submitBtn", btnText, true);
+
+  //// call endpoint //////
+  _callRawEndPoints({
+    url: `parent/auth/login`,
+    formData,
+  })
     .then((response) => {
-  if (response.success) {
-      localStorage.setItem("parentSessionData", JSON.stringify(response));
-      _showLoader("Login Successful. Redirecting, please wait…");
-      window.location.href = parentPortalUrl;
-      _btnDisable("submitBtn", btnText, false);
-		} else {
-			_btnDisable("submitBtn", btnText, false);
-      _hideLoader();
-			_showCustomConfirm({
-				title: "PROCEED LOGIN",
-				message: response.message,
-				alertType: "warning",
-				trueActionBtnText: "OK",
-        closeOnOverlayClick: true,
-			});
-		}
+      if (response.success) {
+        localStorage.setItem("parentSessionData", JSON.stringify(response));
+        _showLoader("Login Successful. Redirecting, please wait…");
+        window.location.href = parentPortalUrl;
+        _btnDisable("submitBtn", btnText, false);
+      } else {
+        _btnDisable("submitBtn", btnText, false);
+        _hideLoader();
+        _showCustomConfirm({
+          title: "Invalid OTP",
+          message: response.message,
+          alertType: "error",
+          trueActionBtnText: "OK",
+          closeOnOverlayClick: true,
+        });
+      }
     })
     .catch((error) => {
-		console.error("Error:", error);
-		_callAjaxError(() => _proceedToLoginCallback(formData)); // retry if needed
-		_btnDisable("submitBtn", btnText, false);
-    _hideLoader();
+      console.error("Error:", error);
+      _callAjaxError(() => _proceedToLoginCallback(formData)); // retry if needed
+      _btnDisable("submitBtn", btnText, false);
+      _hideLoader();
     });
 }
